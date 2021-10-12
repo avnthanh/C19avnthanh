5c1110d04b8f67084757b92fb0b27b2d0f18f4c3
avnthanh <71309713+avnthanh@users.noreply.github.com>
2021-10-13 02:45:23 +1100

    W5 1

--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/CountryAdapter.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/CountryAdapter.java
@@ -3,69 +3,137 @@ package au.edu.unsw.infs3634.covid19tracker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+// Adapter class that connects data set to the recycler view
 public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
-    private List<Country> mCountries;
+    private List<Country>  mCountries;
     private ClickListener mListener;
 
-    CountryAdapter(List<Country> countries, ClickListener listener) {
+    //    Initialise the dataset of the Adapter
+    CountryAdapter(List<Country> countries, ClickListener listener){
         this.mCountries = countries;
         this.mListener = listener;
     }
 
+    // Allows click events to be caught
     public interface ClickListener {
         void onProductClick(View view, int countryID);
     }
 
+    // Inflate the row layout from xml when needed (just the view, no data)
     @NonNull
     @Override
     public CountryAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //here, we defined a view by the item_row_layout and returned it to the viewholder
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_layout, parent, false);
         return new MyViewHolder(view, mListener);
     }
 
+    // Bind the data to the TextView elements in each row
     @Override
     public void onBindViewHolder(@NonNull CountryAdapter.MyViewHolder holder, int position) {
-        //Takes the position of a country in an arraylist and displays it
         final Country country = mCountries.get(position);
         int countryID = position;
         holder.countryName.setText(country.getCountry());
-        holder.newCases.setText("+ " + country.getNewConfirmed());
+        holder.newCases.setText(country.getNewConfirmed());
         holder.totalCases.setText(country.getTotalConfirmed());
         holder.itemView.setTag(countryID);
     }
 
+    // Total number of rows in the list
     @Override
     public int getItemCount() {
         return mCountries.size();
     }
 
+    // Create view holder. The view holder has two text view elements
     class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public TextView countryName, newCases, totalCases;
+        private TextView countryName, newCases, totalCases;
         private ClickListener listener;
 
         public MyViewHolder(@NonNull View itemView, ClickListener listener) {
             super(itemView);
             this.listener = listener;
             itemView.setOnClickListener(MyViewHolder.this);
-
-            //the view is passed from onCreateViewHolder and the textviews are linked and instantiated by id
-            countryName.findViewById(R.id.tvCountryName);
-            newCases.findViewById(R.id.tvNewCases);
-            totalCases.findViewById(R.id.tvTotalCases);
+            countryName = itemView.findViewById(R.id.tvCountryName);
+            newCases = itemView.findViewById(R.id.tvNewCases);
+            totalCases = itemView.findViewById(R.id.tvTotalCases);
         }
 
         @Override
-        public void onClick(View view) {
-            listener.onProductClick(view, (Integer) view.getTag());
+        public void onClick(View v) {
+            listener.onProductClick(v, (Integer) v.getTag());
         }
     }
 
 }
+
+//public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
+//    private List<Country> mCountries;
+//    private ClickListener mListener;
+//
+//    CountryAdapter(List<Country> countries, ClickListener listener) {
+//        this.mCountries = countries;
+//        this.mListener = listener;
+//    }
+//
+//    public interface ClickListener {
+//        void onProductClick(View view, int countryID);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public CountryAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        //here, we defined a view by the item_row_layout and returned it to the viewholder
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_layout, parent, false);
+//        return new MyViewHolder(view, mListener);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull CountryAdapter.MyViewHolder holder, int position) {
+//        //Takes the position of a country in an arraylist and displays it
+//        final Country country = mCountries.get(position);
+//        int countryID = position;
+//        holder.countryName.setText(country.getCountry());
+//        holder.newCases.setText("+ " + country.getNewConfirmed());
+//        holder.totalCases.setText(country.getTotalConfirmed());
+//        holder.itemView.setTag(countryID);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return mCountries.size();
+//    }
+//
+//    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//        public TextView countryName, newCases, totalCases;
+//        private ClickListener listener;
+//
+//        public MyViewHolder(@NonNull View itemView, ClickListener listener) {
+//            super(itemView);
+//            this.listener = listener;
+//            itemView.setOnClickListener(MyViewHolder.this);
+//
+//            //the view is passed from onCreateViewHolder and the textviews are linked and instantiated by id
+//            countryName.findViewById(R.id.tvCountryName);
+//            newCases.findViewById(R.id.tvNewCases);
+//            totalCases.findViewById(R.id.tvTotalCases);
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//            listener.onProductClick(view, (Integer) view.getTag());
+//        }
+//    }
+//
+//}
--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private List<Country> mCountries = new ArrayList<>();
-    private CountryAdapter cAdapter;
+    private CountryAdapter mAdapter;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -21,8 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Instantiate Recyclerview
         mRecyclerView = findViewById(R.id.recycleView);
+        // Set setHasFixedSize true if contents of the adapter does not change it's height or the width
         mRecyclerView.setHasFixedSize(true);
+
+        // Specify linear layout manager for RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(layoutManager);
 
@@ -30,12 +39,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProductClick(View view, int countryID) {
                 final Country country = mCountries.get(countryID);
-                Toast.makeText(getApplicationContext(), country.getCountry() + "\nNew Cases = " +country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
+                // Display details for the selected RecyclerView item (product on the list)
+                Toast.makeText(getApplicationContext(), country.getCountry()+"\nNew Cases = $"+country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
             }
         };
-
-        cAdapter = new CountryAdapter(mCountries, listener);
-        mRecyclerView.setAdapter(cAdapter);
+        // Instantiate adapter
+        mAdapter = new CountryAdapter(mCountries, listener);
+        // Set the adapter for the recycler view
+        mRecyclerView.setAdapter(mAdapter);
     }
-
 }
+
+//public class MainActivity extends AppCompatActivity {
+//    private List<Country> mCountries = new ArrayList<>();
+//    private CountryAdapter cAdapter;
+//    private RecyclerView mRecyclerView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mRecyclerView = findViewById(R.id.recycleView);
+//        mRecyclerView.setHasFixedSize(true);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+//        mRecyclerView.setLayoutManager(layoutManager);
+//
+//        CountryAdapter.ClickListener listener = new CountryAdapter.ClickListener() {
+//            @Override
+//            public void onProductClick(View view, int countryID) {
+//                final Country country = mCountries.get(countryID);
+//                Toast.makeText(getApplicationContext(), country.getCountry() + "\nNew Cases = " +country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//
+//        cAdapter = new CountryAdapter(mCountries, listener);
+//        mRecyclerView.setAdapter(cAdapter);
+//    }
+//
+//}
