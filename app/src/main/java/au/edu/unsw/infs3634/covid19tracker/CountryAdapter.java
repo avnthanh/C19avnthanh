c4d8eb59f6a94b73a2135643573bc4a83d89aee0
avnthanh <71309713+avnthanh@users.noreply.github.com>
2021-10-26 19:15:08 +1100

    W5 2

--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/CountryAdapter.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/CountryAdapter.java
@@ -14,16 +14,54 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 // Adapter class that connects data set to the recycler view
-public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> implements Filterable{
     private List<Country>  mCountries;
+    private List<Country> mCountriesFiltered;
     private ClickListener mListener;
 
     //    Initialise the dataset of the Adapter
     CountryAdapter(List<Country> countries, ClickListener listener){
         this.mCountries = countries;
+        this.mCountriesFiltered = countries;
         this.mListener = listener;
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            //Hamid's CharSequence was called constraint for some reason, and his FilterResults as results
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                //convert charSequence to String
+                String charString = charSequence.toString();
+                if(charString.isEmpty()) {
+                    mCountriesFiltered = mCountries;
+                } else {
+                    ArrayList<Country> filteredList = new ArrayList<>();
+                    for(Country country : mCountries) {
+                        if(country.getCountry().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(country);
+                        }
+                    }
+                    mCountriesFiltered = filteredList;
+                }
+
+                //instantiate an instance of FilterResults. Note that this is different from publishResults parameter.
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mCountriesFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mCountriesFiltered = (ArrayList<Country>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     // Allows click events to be caught
@@ -42,18 +80,29 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHo
     // Bind the data to the TextView elements in each row
     @Override
     public void onBindViewHolder(@NonNull CountryAdapter.MyViewHolder holder, int position) {
-        final Country country = mCountries.get(position);
+//        final Country country = mCountries.get(position);
+        final Country country = mCountriesFiltered.get(position);
         int countryID = position;
+
+        //converting Integer value of Country methods to String so setText can work
+        int newConfirmedInt = country.getNewConfirmed();
+        String newConfirmedString = String.valueOf(newConfirmedInt);
+        int totalConfirmedInt = country.getTotalConfirmed();
+        String totalConfirmedString = String.valueOf(totalConfirmedInt);
+
+        //binding the data to textviews
         holder.countryName.setText(country.getCountry());
-        holder.newCases.setText(country.getNewConfirmed());
-        holder.totalCases.setText(country.getTotalConfirmed());
+        holder.newCases.setText("+ " + newConfirmedString);
+        holder.totalCases.setText(totalConfirmedString);
+//        holder.newCases.setText(country.getNewConfirmed());
+//        holder.totalCases.setText(country.getTotalConfirmed());
         holder.itemView.setTag(countryID);
     }
 
     // Total number of rows in the list
     @Override
     public int getItemCount() {
-        return mCountries.size();
+        return mCountriesFiltered.size();
     }
 
     // Create view holder. The view holder has two text view elements
@@ -76,64 +125,25 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHo
         }
     }
 
+    //Sorting method
+    public void sort(final int sortMethod) {
+        if (mCountriesFiltered.size() > 0) {
+            Collections.sort(mCountriesFiltered, new Comparator<Country>() {
+                @Override
+                public int compare(Country t1, Country t2) {
+                    if(sortMethod == 1) {
+                        //compare the country names of t1 and t2
+                        return t1.getCountry().compareTo(t2.getCountry());
+                    } else if (sortMethod == 2)
+                        //compare the new cases value of t1 and t2
+                        return String.valueOf(t1.getNewConfirmed()).compareTo(String.valueOf(t2.getNewConfirmed()));
+                    //sort by name if no other options
+                    return t1.getCountry().compareTo(t2.getCountry());
+                }
+            });
+        }
+        notifyDataSetChanged();
+    }
+
 }
 
-//public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
-//    private List<Country> mCountries;
-//    private ClickListener mListener;
-//
-//    CountryAdapter(List<Country> countries, ClickListener listener) {
-//        this.mCountries = countries;
-//        this.mListener = listener;
-//    }
-//
-//    public interface ClickListener {
-//        void onProductClick(View view, int countryID);
-//    }
-//
-//    @NonNull
-//    @Override
-//    public CountryAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        //here, we defined a view by the item_row_layout and returned it to the viewholder
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_layout, parent, false);
-//        return new MyViewHolder(view, mListener);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull CountryAdapter.MyViewHolder holder, int position) {
-//        //Takes the position of a country in an arraylist and displays it
-//        final Country country = mCountries.get(position);
-//        int countryID = position;
-//        holder.countryName.setText(country.getCountry());
-//        holder.newCases.setText("+ " + country.getNewConfirmed());
-//        holder.totalCases.setText(country.getTotalConfirmed());
-//        holder.itemView.setTag(countryID);
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return mCountries.size();
-//    }
-//
-//    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-//        public TextView countryName, newCases, totalCases;
-//        private ClickListener listener;
-//
-//        public MyViewHolder(@NonNull View itemView, ClickListener listener) {
-//            super(itemView);
-//            this.listener = listener;
-//            itemView.setOnClickListener(MyViewHolder.this);
-//
-//            //the view is passed from onCreateViewHolder and the textviews are linked and instantiated by id
-//            countryName.findViewById(R.id.tvCountryName);
-//            newCases.findViewById(R.id.tvNewCases);
-//            totalCases.findViewById(R.id.tvTotalCases);
-//        }
-//
-//        @Override
-//        public void onClick(View view) {
-//            listener.onProductClick(view, (Integer) view.getTag());
-//        }
-//    }
-//
-//}
--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //this passes the data from Country class to mCountries, which then is passed to Adapter
+        mCountries = Country.getCountries();
+
         // Instantiate Recyclerview
         mRecyclerView = findViewById(R.id.recycleView);
         // Set setHasFixedSize true if contents of the adapter does not change it's height or the width
@@ -38,43 +41,55 @@ public class MainActivity extends AppCompatActivity {
         CountryAdapter.ClickListener listener = new CountryAdapter.ClickListener() {
             @Override
             public void onProductClick(View view, int countryID) {
-                final Country country = mCountries.get(countryID);
+                Country country = mCountries.get(countryID);
                 // Display details for the selected RecyclerView item (product on the list)
-                Toast.makeText(getApplicationContext(), country.getCountry()+"\nNew Cases = $"+country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), country.getCountry()+"\nNew Cases = + "+country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
             }
         };
+
         // Instantiate adapter
         mAdapter = new CountryAdapter(mCountries, listener);
         // Set the adapter for the recycler view
         mRecyclerView.setAdapter(mAdapter);
     }
-}
 
-//public class MainActivity extends AppCompatActivity {
-//    private List<Country> mCountries = new ArrayList<>();
-//    private CountryAdapter cAdapter;
-//    private RecyclerView mRecyclerView;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        mRecyclerView = findViewById(R.id.recycleView);
-//        mRecyclerView.setHasFixedSize(true);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-//        mRecyclerView.setLayoutManager(layoutManager);
-//
-//        CountryAdapter.ClickListener listener = new CountryAdapter.ClickListener() {
-//            @Override
-//            public void onProductClick(View view, int countryID) {
-//                final Country country = mCountries.get(countryID);
-//                Toast.makeText(getApplicationContext(), country.getCountry() + "\nNew Cases = " +country.getNewConfirmed(), Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//
-//        cAdapter = new CountryAdapter(mCountries, listener);
-//        mRecyclerView.setAdapter(cAdapter);
-//    }
-//
-//}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate menu, instantiate and assign the resource and menu instance
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        //instantiate searchview object and set it to layout id
+        SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mAdapter.getFilter().filter(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                mAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //using an instance of MenuItem and assigning its value depending on what the user clicked
+        switch (item.getItemId()) {
+            case R.id.sortName:
+                //assigns the value of sortMethod and passes it to sort method in CountryAdapter
+                mAdapter.sort(1);
+                return true;
+            case R.id.sortNewCases:
+                mAdapter.sort(2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
